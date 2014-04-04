fd7b34d2317883879f47789ac452f48b11d9a107
Satoshi Yamamoto <satoshi@yamamoto.name>
2014-04-04 10:02:12 +0900

    add stream example.

--- src/main/java/name/yamamoto/satoshi/java8/stream/Main.java
+++ src/main/java/name/yamamoto/satoshi/java8/stream/Main.java
@@ -3,7 +3,9 @@ package name.yamamoto.satoshi.java8.stream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
+import name.yamamoto.satoshi.java8.Person;
 import name.yamamoto.satoshi.java8.PersonRepository;
 
 public class Main {
@@ -34,6 +36,18 @@ public class Main {
 			.filter(p -> p.isCaptain())
 			.forEach(System.out::println);
 		
+		// Max Example, result => サイード横田絵玲奈
+		Person maxPerson = repository.findAll().stream()
+			.max((p1, p2) -> p1.getName().length() - p2.getName().length())
+			.get();
+		System.out.printf("most long name is %s%n", maxPerson);
+		
+		// Min Example, result => 岩田華怜
+		Person minPerson = repository.findAll().stream()
+			.min((p1, p2) -> p1.getAge() - p2.getAge())
+			.get();
+		System.out.printf("most younger is %s%n", minPerson);
+		
 		// Map Example, result => 4,5,6,7
 		List<Integer> numbers = Arrays.asList(new Integer[]{3, 4, 5, 6});
 		numbers.stream()
@@ -43,7 +57,21 @@ public class Main {
 		// Reduce Example, result => 18
 		int sum = numbers.stream()
 			.reduce((i, j) -> i + j).get();
-		System.out.println(sum);
+		System.out.printf("sum = %d%n", sum);
+		
+		// Distinct and Sum Example, 
+		sum = IntStream.of(new int[]{1, 2, 2, 3, 3, 3})
+			.distinct()
+			.sum();
+		System.out.printf("sum = %d%n", sum);
+
+		// Distinct and Sum Example, 
+		double avr = IntStream.of(new int[]{1, 2, 2, 3, 3, 3})
+			.distinct()
+			.average()
+			.getAsDouble();
+		System.out.printf("avr = %f%n", avr);
+
 	}
 	
 	public static void print(List<?> list, String message) {
